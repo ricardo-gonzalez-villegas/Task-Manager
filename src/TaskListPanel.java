641983cd741c4cbe2aed6529b72b1fe8e95cf19a
Eric Flick <flick.ej@gmail.com>
2022-05-01 12:35:30 +0800

    Add deletion callback to TaskListPanel. TaskPanel now calls deletion callback when item deleted

--- src/TaskListPanel.java
+++ src/TaskListPanel.java
@@ -3,16 +3,37 @@ import java.awt.*;
 
 public class TaskListPanel {
    JPanel panel = new JPanel();
+
+   TaskList taskList;
    
    public TaskListPanel(TaskList list){
        int rows = list.getTaskCount();
        this.panel.setLayout(new GridLayout(rows, 1));
-       for (Task task: list.getTaskList()) {
-           TaskPanel taskPanel = new TaskPanel(task);
-           this.panel.add(taskPanel.getPanel());
-       }
+       this.taskList = list;
+
+       addTasksToPanel();
    }
-   public JPanel getPanel(){
+
+    private void addTasksToPanel() {
+        for (Task task: this.taskList.getTaskList()) {
+            TaskPanel taskPanel = new TaskPanel(task, this::onItemDeleted);
+            this.panel.add(taskPanel.getPanel());
+        }
+    }
+
+    public JPanel getPanel(){
        return this.panel;
+   }
+
+   public void onItemDeleted() {
+       // Remove everything from the panel
+       this.panel.removeAll();
+
+       // Re-add all the tasks to the panel
+       this.addTasksToPanel();
+
+       // Repaint panel with newly added components
+       this.panel.revalidate();
+       this.panel.repaint();
    }
 }
